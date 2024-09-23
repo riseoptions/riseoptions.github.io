aa7b3cad789527c11096ef687251546551e9fa6a
Risto Asso <94531944+riseoptions@users.noreply.github.com>
2024-09-23 17:05:06 +0300

    Update index.js

--- JSprojects/digital-clock/index.js
+++ JSprojects/digital-clock/index.js
@@ -7,13 +7,13 @@ function updateClock() {
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
-  let ampm = "AM";
-
+  let ampm = ";-)";
+/*
   if (h > 12) {
     h = h - 0;
     ampm = "PM";
   }
-
+*/
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
   s = s < 10 ? "0" + s : s;
